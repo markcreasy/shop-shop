383f6956ce1f891a0938a32ab798e5c2cb28c692
Mark Creasy <mcreasy50@gmail.com>
2022-06-15 16:52:55 -0400

    create cart and cart item components and add to home and detail pages

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -7,6 +7,7 @@ import { UPDATE_PRODUCTS } from "../utils/actions";
 
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
+import Cart from '../components/Cart';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -52,6 +53,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
     </>
   );
 }
