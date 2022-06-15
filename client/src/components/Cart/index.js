4040e94cb9fe10bafac81055939204edb5e2cb37
Mark Creasy <mcreasy50@gmail.com>
2022-06-15 16:56:47 -0400

    toggle cart

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART } from '../../utils/actions';
 
 const Cart = () => {
+
+  const [state, dispatch] = useStoreContext();
+
+  function toggleCart() {
+    dispatch({ type: TOGGLE_CART });
+  }
+  
+  if (!state.cartOpen) {
+    return (
+      <div className="cart-closed" onClick={toggleCart}>
+        <span
+          role="img"
+          aria-label="trash">🛒</span>
+      </div>
+    );
+  }
+
   return (
     <div className="cart">
-      <div className="close">[close]</div>
+      <div className="close" onClick={toggleCart}>[close]</div>
       <h2>Shopping Cart</h2>
       <div>
           <CartItem item={{name:'Camera', image:'camera.jpg', price:5, purchaseQuantity:3}} />
