c07eeeaa5e903439bc0f77794abed7ba7cca5f4d
Mark Creasy <mcreasy50@gmail.com>
2022-06-16 10:50:35 -0400

    stripe order processing

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,15 +1,25 @@
 import React, { useEffect } from "react";
+// custom components
 import CartItem from '../CartItem';
+// authentication
 import Auth from '../../utils/auth';
+// custom css
 import './style.css';
+// state
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { useLazyQuery } from '@apollo/client';
+// stripe
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
 
+  // state variables
   const [state, dispatch] = useStoreContext();
-
+  // cart load && updates
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
@@ -21,10 +31,12 @@ const Cart = () => {
     }
   }, [state.cart.length, dispatch]);
 
+  // show/hide cart via global state
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
   }
 
+  // calculate cart total price
   function calculateTotal() {
     let sum = 0;
     state.cart.forEach(item => {
@@ -32,6 +44,34 @@ const Cart = () => {
     });
     return sum.toFixed(2);
   }
+
+  // Checkout query
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+  // checkout submit listener
+  function submitCheckout() {
+    const productIds = [];
+    // collect product ids for stripe submission
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+    // submit productids array to stripe
+    getCheckout({
+      variables: { products: productIds }
+    });
+  }
+
+  // when Query Checkout Data updates
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+      console.log(data);
+    }
+  }, [data]);
 
   if (!state.cartOpen) {
     return (
@@ -56,9 +96,9 @@ const Cart = () => {
             <strong>Total: ${calculateTotal()}</strong>
             {
               Auth.loggedIn() ?
-                <button>
-                  Checkout
-                </button>
+              <button onClick={submitCheckout}>
+                Checkout
+              </button>
                 :
                 <span>(log in to check out)</span>
             }
